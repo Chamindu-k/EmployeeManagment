7a5cb848fc1f64b2688485a6c0a8695859ced31d
Chamindu-k <chamindukaushalyaweerakkodi@gmail.com>
2026-05-09 15:48:55 +0530

    added furniture brand and type

--- src/main/java/com/chamindu/demo/model/dto/FurnitureDTO.java
+++ src/main/java/com/chamindu/demo/model/dto/FurnitureDTO.java
@@ -5,6 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
 import java.util.UUID;
 
 /**
@@ -17,4 +18,6 @@ public class FurnitureDTO {
     private Long furnitureId ;
     private String name;
     private String department;
+    private List<FurnitureTypeDTO> type;
+
 }
--- src/main/java/com/chamindu/demo/model/entity/Furniture.java
+++ src/main/java/com/chamindu/demo/model/entity/Furniture.java
@@ -3,6 +3,8 @@ package com.chamindu.demo.model.entity;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 /**
@@ -16,8 +18,18 @@ import java.util.UUID;
 public class Furniture {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id ;
+    private Long id;
     private String name;
     private String department;
 
+    @OneToMany(mappedBy = "furniture",
+            cascade = CascadeType.ALL,
+            orphanRemoval = true)
+    private List<FurnitureType> type = new ArrayList<>();
+
+    //    keep both sides in sync(important)
+    public void addType(FurnitureType type){
+       type.add(type);
+        type.setFurniture(this);
+    }
 }
